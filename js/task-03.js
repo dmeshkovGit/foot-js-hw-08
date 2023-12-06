32b7cccc07a6b785b749c4027c635e27c3372907
dmeshkovGit <140300183+dmeshkovGit@users.noreply.github.com>
2023-12-07 00:22:10 +0200

    done

--- js/task-03.js
+++
@@ -1,9 +0,0 @@
-const inputName = document.querySelector("#name-input");
-const outputName = document.querySelector("#name-output");
-inputName.addEventListener("input", (event) => {
-    if (event.currentTarget.value.trim() !== "") {
-         outputName.textContent = event.currentTarget.value.trim()
-    } else {
-        outputName.textContent = "Anonymous";
-    }
-});
